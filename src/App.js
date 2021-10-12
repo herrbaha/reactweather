72c0515bf51e020ca91ae0720f2238a96abaebdb
bahadir <baha_i@hotmail.com>
2021-10-12 14:09:29 +0200

    in search div ein input und button erstellt erstellt

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import './App.css';
-API_Key: '979bcb3167224872adb115058211210';
-const url = 'http://api.weatherapi.com/v1/forecast.json?key=&q=London&days=3&aqi=no&alerts=no';
+import { useState } from "react";
+import "./App.css";
+API_Key: "979bcb3167224872adb115058211210";
+const url =
+  "http://api.weatherapi.com/v1/forecast.json?key=&q=London&days=3&aqi=no&alerts=no";
+
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <div className="search">
+        <input type="text" placeholder="Search a City" />
+        <button>Search</button>
+      </div>
     </div>
   );
 }
