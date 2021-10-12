27445100f6cf13aa887438d957b2470d3a8545b6
bahadir <baha_i@hotmail.com>
2021-10-12 14:02:03 +0200

    apikey und link erstellt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-
+API_Key: '979bcb3167224872adb115058211210';
+const url = 'http://api.weatherapi.com/v1/forecast.json?key=&q=London&days=3&aqi=no&alerts=no';
 function App() {
   return (
     <div className="App">
