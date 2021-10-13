66d9aa9a1fb50d66e454e56ceb4b356c38a72b96
bahadir <baha_i@hotmail.com>
2021-10-13 10:21:04 +0200

    fetch problem4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Weatherresult from "./components/Weatherresult";
+import {getDefaultNormalizer} from '@testing-library/react'
 
 
  
@@ -9,14 +10,21 @@ function App() {
   const APP_KEY: "979bcb3167224872adb115058211210" 
 
   let cityinput =""
-  
-  const [wheatherData, setWheatherData] = useState([]);
 
-  const citytext = (e) => {
-   e.preventDefault();
-   cityinput= e.target.value;
-   console.log(cityinput);
+  const [wheatherData, setWheatherData] = useState([]);
+  
+  function citytext(){
+    document.querySelector("input").addEventListener("input", (e) =>{
+      e.preventDefault();
+      cityinput = e.target.value;
+      console.log(cityinput)
+    })
   }
+  // const citytext = (e) => {
+  //  e.preventDefault();
+  //  cityinput= e.target.value;
+  //  console.log(cityinput);
+  // }
 
   async function getdata(value) {
     const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${APP_KEY}&q=${value}&days=3&aqi=no&alerts=no`)
@@ -27,10 +35,10 @@ function App() {
     <div>
       <div className="search">
         <input type="text" placeholder="Search a City" onChange={citytext}/>
-        <button onClick={() => getdata()} >Search</button>
+        <button onClick={() => getdata(cityinput)} >Search</button>
         
       </div>
-      <Weatherresult/>
+      {/* <Weatherresult/> */}
     </div>
   );
 }
