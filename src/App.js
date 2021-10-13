1416a2de55ff0226329de3f96b7b03d41c8d1a5a
bahadir <baha_i@hotmail.com>
2021-10-13 10:01:55 +0200

    fetch problem

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 // import { useState } from "react";
 import "./App.css";
-
-// const url =
-//   "http://api.weatherapi.com/v1/forecast.json?key=&q=London&days=3&aqi=no&alerts=no";
 import Weatherresult from "./components/Weatherresult";
 
+
+const API_Key: "979bcb3167224872adb115058211210"; 
+
 function App() {
-  // const API_Key: "979bcb3167224872adb115058211210"; 
+  
   let cityinput ="";
+  const [data, setData] = useState([]);
 
   const citytext = (e) => {
    e.preventDefault();
    cityinput= e.target.value;
-   console.log(cityinput);
+  //  console.log(cityinput);
   }
-  const getdata=(value) => {
-  console.log("buton basildi");
+  async function getdata(value) {
+    const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_Key}&q=${value}&days=3&aqi=no&alerts=no`)
+    const result = await data.json();
+    console.log(result);
   }
   return (
     <div>
       <div className="search">
         <input type="text" placeholder="Search a City" onChange={citytext}/>
-        <button onClick={() => getdata(cityinput)} >Search</button>
+        <button onClick={() => getdata()} >Search</button>
         
       </div>
       <Weatherresult/>
