69d88aebfa67514083d3faa740c3d70f00b2e5e8
bahadir <baha_i@hotmail.com>
2021-10-13 21:45:31 +0200

    input geändert

--- src/App.js
+++ src/App.js
@@ -21,17 +21,31 @@ function App() {
   }
 
   async function getdata(value) {
-    const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${value}&days=7&aqi=no&alerts=no`)
+    const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${value}&days=3&aqi=no&alerts=no`)
     const result = await data.json();
     setWheatherData(result.forecast.forecastday);
+    // console.log(result)
+  }
+  const handleSubmit = (e) => {
+   e.preventDefault();
+   getdata(citytext)
   }
   return (
     <div>
-      <div className="search">
+      {/* <div className="search">
+      
         <input type="text" placeholder="Search a City" onChange={citytext}/>
         <button onClick={() => getdata(cityinput)} >Search</button>
-        
-      </div>
+    
+      </div> */}
+
+      <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Search a City" onChange={citytext}/>
+      <button type="submit">Search</button>
+
+      </form>
+      
+
       {wheatherData.map((item, index) => (
         <Weatherresult key={index} date={item.date} icon={item.day.condition.icon} condition={item.day.condition.text} temp={item.day.avgtemp_c} humidity={item.day.avghumidity}/>
       ))}
