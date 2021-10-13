8df6ed52522edc01c1955960b3d641ed7d0f03e2
bahadir <baha_i@hotmail.com>
2021-10-13 12:22:36 +0200

    fetch problem6

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
 import { useState } from "react";
 import "./App.css";
 import Weatherresult from "./components/Weatherresult";
-import {getDefaultNormalizer} from '@testing-library/react'
+// import {getDefaultNormalizer} from '@testing-library/react'
 
 
  
 
 function App() {
-  const APP_KEY: "979bcb3167224872adb115058211210" 
+  let APP_KEY: "979bcb3167224872adb115058211210";
 
-  let cityinput =""
+  let cityinput =""; 
 
   const [wheatherData, setWheatherData] = useState([]);
   
-  function citytext(){
-    document.querySelector("input").addEventListener("input", (e) =>{
-      e.preventDefault();
-      cityinput = e.target.value;
-      console.log(cityinput)
-    })
-  }
-  // const citytext = (e) => {
-  //  e.preventDefault();
-  //  cityinput= e.target.value;
-  //  console.log(cityinput);
+  // function citytext(){
+  //   document.querySelector("input").addEventListener("input", (e) =>{
+  //     e.preventDefault();
+  //     cityinput = e.target.value;
+  //     console.log(cityinput)
+  //   })
   // }
+  const citytext = (e) => {
+  
+   e.preventDefault();
+   cityinput= e.target.value;
+   console.log(cityinput);
+  }
 
   async function getdata(value) 
   {
-    const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${APP_KEY}&q=${value}&days=3`)
+    const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${APP_KEY}&q=London&days=3`)
     const result = await data.json();
-    setWheatherData(result.forecast.forecastday)
+    setWheatherData(result.forecast)
+    console.log(result.forecast)
   }
   return (
     <div>
