3d969f4cb181cda68bfecb350406e54df6ebe354
bahadir <baha_i@hotmail.com>
2021-10-13 22:51:46 +0200

    error state erstellt

--- src/App.js
+++ src/App.js
@@ -14,22 +14,32 @@ function App() {
 
   const [wheatherData, setWheatherData] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [cityName, setCityName] = useState("");
+  const [error , setError] = useState(false);
+  const [loading, setLoading]= useState(false);
 
   const citytext = (e) => {
    e.preventDefault();
-  //  cityinput= e.target.value;
   setInputValue(e.target.value);
   
   }
 
   async function getdata(value) {
-    const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${value}&days=3&aqi=no&alerts=no`)
+    loading(true);
+    try {
+      const data = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${value}&days=3&aqi=no&alerts=no`)
     const result = await data.json();
     setWheatherData(result.forecast.forecastday);
-    // console.log(result)
+    setError(false);
+    setLoading(false);
+    } catch (e) {
+     setError(true)
+     setLoading(false);
+    }
   }
   const handleSubmit = (e) => {
    e.preventDefault();
+   setCityName(inputValue);
    getdata(inputValue)
    setInputValue("")
    
@@ -37,7 +47,9 @@ function App() {
    
   }
   return (
+    
     <div>
+      
       {/* <div className="search">
       
         <input type="text" placeholder="Search a City" onChange={citytext}/>
@@ -45,16 +57,24 @@ function App() {
     
       </div> */}
 
+      
       <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Search a City" value={inputValue} onChange={citytext}/>
       <button type="submit">Search</button>
 
       </form>
-      
+      {!loading && error ? <div>birseyler yanlis gitti</div> : (<div><h2>{cityName}'s last tree wheather Result </h2>       
+
+{wheatherData.map((item, index) => (
+  <Weatherresult key={index} date={item.date} icon={item.day.condition.icon} condition={item.day.condition.text} temp={item.day.avgtemp_c} humidity={item.day.avghumidity}/>
+))}
+
+</div>)}
+      {/* <h2>{cityName}'s last tree wheather Result </h2>       
 
       {wheatherData.map((item, index) => (
         <Weatherresult key={index} date={item.date} icon={item.day.condition.icon} condition={item.day.condition.text} temp={item.day.avgtemp_c} humidity={item.day.avghumidity}/>
-      ))}
+      ))} */}
     </div>
   );
 }
