31780bc01bf9cae756ce3e0a94fa1e3d260a60a3
bahadir <baha_i@hotmail.com>
2021-10-14 14:01:26 +0200

    reactbbotstrap css geändert

--- src/components/Weatherresult.js
+++ src/components/Weatherresult.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import "./weatherresult.css";
-import Button from 'react-bootstrap/Button';
-import  'bootstrap/dist/css/bootstrap.min.css' ;
+import { Container, Row, Col } from 'react-bootstrap';
+// import  'bootstrap/dist/css/bootstrap.min.css' ;
 
 const Weatherresult = ({date, humidity, temp,icon, condition}) => {
     return (
-      //  <div className="result">
-      //     <h3>{date}</h3>
-      //     <h4>{temp} C</h4>
-      //     <img src={icon} alt=""/>
-      //     <h5>{condition}</h5>
-      //     <h3>{humidity}%</h3> 
-      //  </div>
-       <Container>
+       <Container style={{color:"white"}}>
        <Row>
-         <Col>{date}</Col>
-         <Col>{temp} C</Col>
-         <Col><img src={icon} alt=""/></Col>
-         <Col>{condition}</Col>
+         <Col  style={{color:"40%"}}>{date}</Col>
+         <Col style={{color:"rgb(255, 217, 0)", whiteSpace: "nowrap", fontWeight:"bold"}}>{temp} C</Col>
+         <Col><img style={{height:"60%"}} src={icon} alt=""/></Col>
+         <Col style={{color:"50%"}}>{condition}</Col>
          <Col>{humidity}%</Col>
        </Row>
      </Container>
